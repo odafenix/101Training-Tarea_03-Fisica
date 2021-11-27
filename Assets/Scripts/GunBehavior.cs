a575032a3898b659595d2e7600e83ad7bc60418d
odafenix <68091954+odafenix@users.noreply.github.com>
2021-11-27 16:47:50 -0300

    tweaks 1

--- Assets/Scripts/GunBehavior.cs
+++ Assets/Scripts/GunBehavior.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class GunBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+        public Transform target;
+    public float bulletSpeed = 6f;
+    public float bulletReload = 2f;
+    private bool bulletCooldown;
+    public GameObject bulletPrefab;
+    public Transform bulletOrigin;
+    public float speed = 3f;
+    public bool ignorarEjeY = true;
+    Vector3 directionVector = Vector3.zero;
+    Vector3 originalPosition;
+    private void Start() {
+        originalPosition = gameObject.transform.position;
+    }
+    private void OnTriggerStay(Collider Player) {
+        
+        if(Player.tag == "Player"){
+            target = Player.transform;
+        }
+        
+
+        if(Player.tag != "Player"){
+            return;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+     void OnTriggerExit(Collider Player)
     {
         
+       if(Player.tag == "Player"){
+            target = null;
+        }
+        if(Player.tag != "Player"){
+            return;
+        }
+    }
+    private void FixedUpdate() {
+        StartCoroutine(ShootAtTarget());
+    }
+    
+    IEnumerator ShootAtTarget(){
+        if (target != null & bulletCooldown == false){
+            Instantiate(bulletPrefab, bulletOrigin.position, bulletOrigin.rotation);
+             bulletCooldown = true;
+             yield return new WaitForSeconds(bulletReload);
+             bulletCooldown = false;
+        }
     }
 }
